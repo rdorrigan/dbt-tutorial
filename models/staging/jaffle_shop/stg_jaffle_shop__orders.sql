a515d1452c12c3ece0b85487426be1e16b987a00
Robert Dorrigan <rdorrigan@gmail.com>
2026-08-17 15:36:45 +0000

    clean up example models

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -2,7 +2,7 @@ select
     id as order_id,
     user_id as customer_id,
     order_date,
-    status
+    status as order_status
 
 from {{ source('jaffle_shop','orders') }}
 -- dbt-tutorial.jaffle_shop.orders
